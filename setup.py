1d06b40dfadca96f93030ba4631741bd75183827
Daniel Gonzalez Fernandez <eldano1995@gmail.com>
2020-12-26 22:02:47 -0500

    feat: Minor changes in setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="activa-campaign-eldano1995",
+    name="active-campaign-eldano1995",
     version="0.0.1",
-    author="Daniel",
-    author_email="eldano1995@gmail.com",
+    author="Umar Kahn",
+    author_email="umar@selfdecode.com",
     description="",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -18,3 +18,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    python_requires='>=3.6',
+)
