825bc1dbcb14145774af893dafd7eb5ad84b4039
Daniel Gonzalez Fernandez <eldano1995@gmail.com>
2020-12-26 23:16:22 -0500

    feat: Minor changes in setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="active-campaign",
     version="1.0.0",
+    name="active-campaign",
+    packages=["active_campaign"],
     long_description=long_description,
     long_description_content_type="text/markdown",
-    packages=setuptools.find_packages(),
+
 )
