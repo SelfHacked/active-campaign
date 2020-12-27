52603c6ea76667fefedd2389ad50d787f461fb28
Daniel Gonzalez Fernandez <eldano1995@gmail.com>
2020-12-26 21:32:35 -0500

    feat: Setup contact_list resource.

---
+++ active_campaign/resources/contact_list.py
@@ -0,0 +1,52 @@
+""" ContactList Resource """
+
+
+import typing
+from ..utils.resource import Resource
+
+
+class ContactList(Resource):
+    """
+    List contact pair. Subscribe a contact to a list or
+    unsubscribe a contact from a list.
+
+    Check docs in:
+    https://developers.activecampaign.com/reference#update-list-status-for-contact
+    """
+
+    def __init__(
+            self,
+            list_id: typing.Optional[int],
+            contact_id: typing.Optional[int],
+            status: typing.Optional[int],
+            **kwargs: typing.Dict,
+    ) -> None:
+        """Initialize the contact list.
+
+        Args:
+            list_id: The id of the list.
+            contact_id: The id of the contact.
+            status: 1 to subscribe the contact and 2 to unsubscribe.
+        """
+        super().__init__(**kwargs)
+        self.list_id = list_id
+        self.contact_id = contact_id
+        self.status = status
+
+    @staticmethod
+    def resource_name() -> str:
+        """Get the name of the API resource.
+
+        Returns:
+            The name of the resource
+        """
+        return 'contactLists'
+
+    @staticmethod
+    def map_field_name_to_attribute() -> typing.Dict:
+        """Map field names to attributes."""
+        return {
+            'list': 'list_id',
+            'contact': 'contact_id',
+            'status': 'status',
+        }
