75d73251cef1c9771d74e86f2bc93d5858c6e73e
Daniel Gonzalez Fernandez <eldano1995@gmail.com>
2020-12-29 18:29:03 -0500

    feat: Empty __init__ files

--- active_campaign/resources/__init__.py
+++
@@ -1,7 +0,0 @@
-""" Export ActiveCampaign resources"""
-
-from .contact_list import ContactList   # noqa: 401
-from .contact_tag import ContactTag  # noqa: 401
-from .contact import Contact  # noqa: 401
-from .marketing_list import MarketingList  # noqa: 401
-from .tag import Tag  # noqa: 401
