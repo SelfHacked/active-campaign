471238432057683d3ef354c7444654dc781988b9
Daniel Gonzalez Fernandez <eldano1995@gmail.com>
2020-12-29 17:59:04 -0500

    feat: Minor changes.

--- active_campaign/__init__.py
+++ active_campaign/__init__.py
@@ -1,5 +1,5 @@
 "Export ActiveCampaignAPI, mock_active_campaign and ActiveCampaign resources"
 
-from .active_campaign_api import ActiveCampaignAPI
-from .active_campaign_mock import mock_active_campaign
-from .resources import Contact, ContactList, ContactTag, Tag, MarketingList
+from .active_campaign_api import ActiveCampaignAPI  # noqa: 401
+from .active_campaign_mock import mock_active_campaign  # noqa: 401
+from .resources import Contact, ContactList, ContactTag, Tag, MarketingList  # noqa: 401
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     version="1.0.0",
-    name="sd_active_campaign",
+    name="active_campaign",
     packages=["active_campaign"],
     long_description=long_description,
     long_description_content_type="text/markdown",
