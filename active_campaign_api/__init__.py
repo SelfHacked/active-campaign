3cc7c41e235365ab3184377fa098d848a8163fee
Daniel Gonzalez Fernandez <eldano1995@gmail.com>
2021-01-14 10:44:49 -0500

    feat: Remove mock_active_campaign from __init__ file

--- active_campaign_api/__init__.py
+++ active_campaign_api/__init__.py
@@ -1,5 +1,4 @@
 "Export ActiveCampaignAPI, mock_active_campaign and ActiveCampaign resources"
 
 from .active_campaign_api import ActiveCampaignAPI  # noqa: 401
-from .active_campaign_mock import mock_active_campaign  # noqa: 401
 from .resources import Contact, ContactList, ContactTag, Tag, MarketingList  # noqa: 401
