41bad2c14bcc10d78317b2ad70b4dbd929a2c851
Daniel Gonzalez Fernandez <eldano1995@gmail.com>
2021-03-29 16:13:21 -0400

    core: Update lib version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    version="1.0.2",
+    version="1.0.3",
     name="active_campaign_api",
     packages=setuptools.find_packages(),
     long_description=long_description,
